a8e6eb02080d5dfd7dbd921ad7acf7d6fc17ed2e
Caitlin <caitlincheer043@gmail.com>
2024-07-15 16:17:38 +0100

    task 4: GET /api/articles/article_id

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
 const { getTopics } = require("./controllers/topics-controllers");
+const { getArticles } = require("./controllers/article-controllers");
 const { getApi } = require("./controllers/api-controllers");
 
 app.get("/api", getApi);
 
 app.get("/api/topics", getTopics);
 
+app.get("/api/articles/:article_id", getArticles);
+
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "incorrect input" });
+  } else next(err);
+})
 app.use((err, req, res, next) => {
   if (err.status && err.msg) {
     res.status(err.status).send({ msg: err.msg });
